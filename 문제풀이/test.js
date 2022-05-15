dd43da8d5b21da29846b9eada44b8564051fa5d7
sangji <sangllee20@gmail.com>
2022-05-15 14:57:57 +0900

    JS 100제 day4: 28~30

--- 문제풀이/test.js
+++ 문제풀이/test.js
@@ -2,12 +2,7 @@ const fs = require("fs");
 const readFileSyncAddress = "test.txt";
 const input = fs.readFileSync(readFileSyncAddress).toString().split("\n");
 
-const name = input[0].split(" ");
-const score = input[1].split(" ");
+const str = input[0];
+const word = input[1];
 
-const obj = {};
-
-for (let i = 0; i < name.length; i++) {
-  obj[name[i]] = parseInt(score[i]);
-}
-console.log(obj);
+console.log(str.indexOf(word));
