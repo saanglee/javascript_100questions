5617cc8ecc8c5ba2e42013102f2787bbac426241
sangji <sangllee20@gmail.com>
2022-05-30 18:07:22 +0900

    JS100제 day4: 34-37

--- 문제풀이/test.js
+++ 문제풀이/test.js
@@ -2,17 +2,11 @@ const fs = require("fs");
 const readFileSyncAddress = "test.txt";
 const input = fs.readFileSync(readFileSyncAddress).toString().split(" ");
 
-let result;
-let inputCopy = JSON.parse(JSON.stringify(input));
-
-inputCopy = inputCopy.sort((a, b) => a - b);
-
-for (let i = 0; i < input.length; i++) {
-  if (input[i] != inputCopy[i]) {
-    result = "NO";
-  } else {
-    result = "YES";
-  }
-}
-
-console.log(result);
+console.log(input);
+let inputToSet = new Set(input);
+console.log(inputToSet);
+console.log(inputToSet[1]); // undefined
+// let array = Array.from(mySet);
+let setToArray = Array.from(inputToSet);
+console.log(setToArray);
+console.log(setToArray[0]);
